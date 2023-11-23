6dab96446b0bc984fecc686125c3d6f0c415dbc8
Josiah Parry <josiah.parry@gmail.com>
2023-11-22 20:09:10 -0500

    use nanoarrow instead of arrow

--- src/rust/src/to.rs
+++ src/rust/src/to.rs
@@ -12,6 +12,62 @@ pub trait ToArrowRobj {
     fn to_arrow_robj(&self) -> Result<Robj>;
 }
 
+// impl<T: ArrowPrimitiveType> ToArrowRobj for PrimitiveArray<T> {
+//     fn to_arrow_robj(&self) -> Result<Robj> {
+//         let data = self.into_data();
+
+//         // take array data and prepare for FFI 
+//         let (ffi_array, ffi_schema) = to_ffi(&data)
+//             .expect("success converting arrow data");
+
+//         // function from {arrow} R package to import an arrow array
+//         let import_from_c = R!("arrow::Array$import_from_c")
+//             .unwrap()
+//             .as_function()
+//             .unwrap();
+
+//         // extract array pointer. we need it as a string to be used by arrow R package
+//         let ffi_array_ptr = &ffi_array as *const FFI_ArrowArray as usize;
+//         let arry_addr_chr = ffi_array_ptr.to_string();
+
+//         // same deal but with the schema 
+//         let ffi_schema_ptr = &ffi_schema as *const FFI_ArrowSchema as usize;
+//         let schema_addr_chr = ffi_schema_ptr.to_string();
+
+//         // run it! 
+//         import_from_c.call(pairlist!(arry_addr_chr, schema_addr_chr))
+//     }
+// }
+
+fn allocate_array() -> Function {
+    R!("nanoarrow::nanoarrow_allocate_array")
+        .expect("`nanoarrow` must be installed")
+        .as_function()
+        .expect("`nanoarrow_allocate_array()` must be available")
+}
+
+fn allocate_schema() -> Function {
+    R!("nanoarrow::nanoarrow_allocate_schema")
+        .expect("`nanoarrow` must be installed")
+        .as_function()
+        .expect("`nanoarrow_allocate_schema()` must be available")
+}
+
+fn move_pointer() -> Function {
+    R!("nanoarrow::nanoarrow_pointer_move")
+        .expect("`nanoarrow` must be installed")
+        .as_function()
+        .expect("`nanoarrow_pointer_move()` must be available")
+}
+
+fn set_array_schema(arr: &Robj, schema: &Robj) {
+    let _ = R!("nanoarrow::nanoarrow_array_set_schema")
+        .expect("`nanoarrow` must be installed")
+        .as_function()
+        .expect("`nanoarrow_array_set_schema()` must be available")
+        .call(pairlist!(arr, schema));
+}
+
 impl<T: ArrowPrimitiveType> ToArrowRobj for PrimitiveArray<T> {
     fn to_arrow_robj(&self) -> Result<Robj> {
         let data = self.into_data();
@@ -19,12 +75,6 @@ impl<T: ArrowPrimitiveType> ToArrowRobj for PrimitiveArray<T> {
         // take array data and prepare for FFI 
         let (ffi_array, ffi_schema) = to_ffi(&data)
             .expect("success converting arrow data");
-
-        // function from {arrow} R package to import an arrow array
-        let import_from_c = R!("arrow::Array$import_from_c")
-            .unwrap()
-            .as_function()
-            .unwrap();
 
         // extract array pointer. we need it as a string to be used by arrow R package
         let ffi_array_ptr = &ffi_array as *const FFI_ArrowArray as usize;
@@ -34,8 +84,17 @@ impl<T: ArrowPrimitiveType> ToArrowRobj for PrimitiveArray<T> {
         let ffi_schema_ptr = &ffi_schema as *const FFI_ArrowSchema as usize;
         let schema_addr_chr = ffi_schema_ptr.to_string();
 
-        // run it! 
-        import_from_c.call(pairlist!(arry_addr_chr, schema_addr_chr))
+        // allocate empty array and schema
+        let arr_to_fill = allocate_array().call(pairlist!())?;
+        let schema_to_fill = allocate_schema().call(pairlist!())?;
+
+        // move pointers
+        let _ = move_pointer().call(pairlist!(arry_addr_chr, &arr_to_fill));
+        let _ = move_pointer().call(pairlist!(schema_addr_chr, &schema_to_fill));
+
+        set_array_schema(&arr_to_fill, &schema_to_fill);
+
+        Ok(arr_to_fill)
     }
 }
 
