e3186672417820cdba241c5eb1a5d2ded16f193d
Josiah Parry <josiah.parry@gmail.com>
2023-11-24 19:48:13 -0500

    impl intoarrowrobj for Box RecordBatchReader

--- src/to.rs
+++ src/to.rs
@@ -204,6 +204,19 @@ impl ToArrowRobj for RecordBatch {
     }
 }
 
+
+impl IntoArrowRobj for Box<dyn RecordBatchReader + Send> {
+    fn into_arrow_robj(self) -> Result<Robj> {
+        let mut stream = FFI_ArrowArrayStream::new(self);
+        let stream_ptr = (&mut stream) as *mut FFI_ArrowArrayStream as usize;
+
+        let stream_to_fill = allocate_array_stream(pairlist!())?;
+        let _ = move_pointer(pairlist!(stream_ptr.to_string(), &stream_to_fill));
+
+        Ok(stream_to_fill)
+    }
+}
+
 /// Convert an Arrow struct to an `Robj` 
 /// 
 /// Consumes `self`. Takes an arrow-rs struct and converts it into
